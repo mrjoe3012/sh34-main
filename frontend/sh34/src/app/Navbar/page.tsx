b99d12a83ff04297096b041aeaf6b0fc82767a64
Erlend Clay <2661388c@student.gla.ac.uk>
2023-11-08 14:35:43 +0000

    implemented Navbar with working logo

--- frontend/sh34/src/app/Navbar/page.tsx
+++ frontend/sh34/src/app/Navbar/page.tsx
@@ -1,21 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import header from "./../images/header-logo.svg"
+
+
 export const Navbar = () => {
     return (
       <header>
         <nav className="h-1/10 top-0">
-          <div className="flex justify-around items-center relative bg-RES_ORANGE p-5">
+          <div className="flex justify-between items-center relative bg-white p-5">
             {/* <ul className=""> */}
               {/* <li>
                 <HomeButton />
               </li> */}
                 <li className='list-none'>
                   <Link href="/home">
-                    <h1 className="text-black text-2xl font-bold align-middle">Home</h1>
+                    <h1 className="text-black text-2xl font-bold align-center"><Image src={header} width={100} height={50}alt="Home" /></h1>
                   </Link>
                 </li>
-                <li className="list-none">
+                {/* <li className="list-none">
                   <Link href="/">
                     <h1 className="text-black text-2xl font-bold">Assets</h1>
                   </Link>
@@ -24,10 +27,10 @@ export const Navbar = () => {
                   <Link href="/">
                     <h1 className="text-black text-2xl font-bold">Example</h1>
                   </Link>
-                </li>
-                <li className="list-none">
-                  <Link href="/inventory">
-                    <h1 className="text-black text-2xl font-bold">Example</h1>
+                </li> */}
+                <li className="list-none ">
+                  <Link href="/profile">
+                    <h1 className="text-black text-2xl font-bold align-right">Profile</h1>
                   </Link>
                 </li>
           {/* </ul> */}
