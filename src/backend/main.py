f09a189843032d78a962bfb5e4cae0a7b56ecb3b
Joe <josephagrane@gmail.com>
2023-11-15 16:02:28 +0000

    #36: fixed some linting issues

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,4 @@
-##########################################
-# This file is the backend's entrypoint. #
-##########################################
+"""This file is the backend's entrypoint."""
 
 from flask import Flask, request
 from backend import generate_graph
@@ -9,15 +7,13 @@ app = Flask(__name__)
 
 def main():
     app.run(host="localhost", port=7575, debug=True)
-    return 
+    return
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     html = generate_graph(request.json)
     return html
-    
+
 
 if __name__ == "__main__":
     main()
-
-
