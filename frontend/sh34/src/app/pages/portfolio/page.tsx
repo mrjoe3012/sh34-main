758fbc3b1166c69a2cdbfcb613ed1cff44c16a71
MatthewW <2679216W@student.gla.ac.uk>
2023-11-15 15:36:19 +0000

    Minor Portfolio Page Changes

--- frontend/sh34/src/app/pages/portfolio/page.tsx
+++ frontend/sh34/src/app/pages/portfolio/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Navbar } from '../../components/Navbar/navbar';
 import Alogo from "./../../images/Alogo.png"
 import UKflag from "./../../images/UKflag.png"
+import WindFarm from "./../../images/windfarmlogo.png"
 
 
 export default function Portfolio() {
@@ -18,8 +19,9 @@ export default function Portfolio() {
 function Body() {
     return (
       <div className="bg-white"> {/*bg-orange-200 could be quite nice */} 
-        <h1 className="text-4xl text-RES_ORANGE font-bold p-6">Home</h1>
+        <h1 className="text-4xl text-RES_ORANGE font-bold p-6">Asset Portfolio</h1>
         <Recents />
+        <Assets />
       </div>
     );
 }
@@ -37,7 +39,9 @@ function Recents(){
 function RecentElements(){
     return(
       <div className="justify-center align-middle">
-        <div className="flex p-2 justify-between mx-20">
+        <div className="flex flex-wrap gap-y-2 p-2 justify-between mx-20">
+          <RecentElementPortfolio />
+          <RecentElementPortfolio />
           <RecentElementPortfolio />
           <RecentElementPortfolio />
           <RecentElementPortfolio />
@@ -48,10 +52,9 @@ function RecentElements(){
     );
 }
 
-  
 function RecentElementPortfolio (){
   return(
-    <div className='container rounded-xl border-4 border-black w-44 min-w-[150px] bg-slate-200 p-1.5'> {/* background as the image on the wireframe */}
+    <div className='container rounded-xl border-4 border-black w-44 min-w-[150px] bg-slate-200 p-1.5' > {/* background as the image on the wireframe */}
           <div className="grid grid-flow-col pb-1">
             <div className='relative'>
                 <Image src={UKflag} alt="test" className="w-10 h-6 self-center object-cover rounded"/>
@@ -83,3 +86,70 @@ function RecentElementPortfolio (){
   )
 }
 
+function Assets(){
+    return(
+      <div className='m-10'>
+        <br></br><br></br>
+        <h1 className='text-2xl text-RES_ORANGE font-bold ml-12'>All Companies</h1>
+            <AssetsHeader />
+            <AssetElement />
+            <AssetElement />
+            <AssetElement />
+            <AssetElement />
+      </div>
+    );
+  }
+
+  function AssetsHeader(){
+    return(
+      <div className=''>
+        <div className='grid grid-cols-8 p-4'>
+          <div className='col-start-2 col-end-3'>
+            <h2 className="text-lg">Company Name</h2>
+          </div>
+          <div className='col-start-3 col-end-4'>
+            <h2 className='text-lg'>No. of Assets</h2>
+          </div>
+        </div>
+        <hr className="h-1 bg-RES_ORANGE boder-none "></hr>
+      </div>
+    );
+  }
+  
+  function AssetElement(){
+    return(
+      <div>
+        <div className='grid grid-cols-8 p-4 items-center'>
+          <div className='relative align-baseline'> 
+            <Image src={Alogo} alt="test" fill={true} className="rounded object-contain p-0"/>
+            <h1 className='text-lg '>LOGO</h1>
+          </div>  
+          <div className="">
+            <h2 className=''>Company</h2>
+          </div>
+          <div>
+              <h2>NUMBER</h2>
+            </div>
+          <div className='col-start-7 col-span-2 px-10'> {/* breakpoints needed to stop it from going over 2 lines on smaller screen OR more elegant solution */}
+            <GeneralButton />
+          </div>
+        </div>
+        <hr className="h-0.5 bg-GRIDLINES border-none mr-4 ml-4"></hr>
+      </div>
+  
+    );
+  }
+  
+  function GeneralButton(){
+    return(
+      <div className='text-center'>
+        <div className="flex rounded-xl p-2 border-black border-2 relative bg-test">
+          {/* <p className='text-white'>little img</p> */}
+          <div className='relative p-0 basis-1/3'> 
+            <Image src={UKflag} alt="test" fill={true} className="self-center rounded object-contain p-0"/>
+          </div>
+          <p className="text-black basis-10/11">View Portfolio</p>
+        </div>
+      </div>
+    );
+  }
