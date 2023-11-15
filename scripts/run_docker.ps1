1d82544ed25a74011bff069a0009d91ff928572f
Joe <josephagrane@gmail.com>
2023-11-15 15:14:30 +0000

    #3: updated run docker script

--- scripts/run_docker.ps1
+++ scripts/run_docker.ps1
@@ -1,4 +1,4 @@
 $PROJECT_ROOT = "$PSScriptRoot/.."
 cd $PROJECT_ROOT
-docker pull joe3012/sh34:latest
-docker run -it -p 3000:3000 -p 7575:7575 -v .:/sh34 joe3012/sh34:latest /root/startup.bash
+docker pull stgit.dcs.gla.ac.uk:5050/team-project-h/2023/sh34/sh34-main:latest
+docker run -it -p 3000:3000 -p 7575:7575 -v .:/sh34 stgit.dcs.gla.ac.uk:5050/team-project-h/2023/sh34/sh34-main:latest /root/startup.bash
