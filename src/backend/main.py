8c38bdae28d0bbc9320fa6ddd8471cc9375ef891
Onur Cay <2662884C@student.gla.ac.uk>
2023-11-15 16:03:52 +0000

    Fixed some lint errors

--- src/backend/main.py
+++ src/backend/main.py
@@ -8,11 +8,13 @@ from backend import generate_graph
 app = Flask(__name__)
 
 def main():
+    """Function to run the webserver"""
     app.run(host="localhost", port=7575, debug=True)
-    return 
+
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
+    """Test function for flask endpoint"""
     html = generate_graph(request.json)
     return html
     
