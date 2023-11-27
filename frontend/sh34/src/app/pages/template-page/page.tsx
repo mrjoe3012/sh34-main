8e503aecb53ba9a9007cf5e4f1e5e25e45cf94de
MatthewW <2679216W@student.gla.ac.uk>
2023-11-27 23:46:55 +0000

    Updated Template Page Plot List Structure. Now Centered and plot box text and icons  now properly alligned.

--- frontend/sh34/src/app/pages/template-page/page.tsx
+++ frontend/sh34/src/app/pages/template-page/page.tsx
@@ -27,7 +27,7 @@ function Body() {
   return (
     <div className="bg-white flex flex-row flex-1 overflow-auto">
         <Sidebar />
-        <div className='basis-[85%] bg-[#e1e3e8]'>
+        <div className='basis-[85%] bg-[#e1e3e8] p-16 flex justify-center overflow-auto'>
             <PlotList />
         </div>
 
@@ -38,7 +38,11 @@ function Body() {
 
 function PlotList(){
     return (
-      <div className='flex flex-col gap-y-5 overflow-auto'>
+      <div className='flex flex-col gap-y-5'>
+        <PlotElement />
+        <PlotElement />
+        <PlotElement />
+        <PlotElement />
         <PlotElement />
         <PlotElement />
       </div>
@@ -48,32 +52,33 @@ function PlotList(){
 
 function PlotElement(){
     return(
-      <div className='flex '>
+      <div className='flex text-black'>
 
         {/* The movement control and delete div */}
-        <div className=''>
-          <Image src={TrashIcon} alt='TrashIcon' />
-          <Image src={UpArrow} alt='UpArrow' />
-          <Image src={DownArrow} alt='DownArrow' />
+        <div className='flex flex-col justify-center items-center'>
+            <Image src={UpArrow} alt='UpArrow' className='w-10 h-10'/>
+            <Image src={DownArrow} alt='DownArrow' className='w-10 h-10'/>
         </div> 
 
         {/* The plot box div */}
-        <div className='w-80 bg-slate-400'>
+        <div className='flex gap-x-5 w-[400px] bg-slate-400 p-3 rounded-xl border-4 border-black'>
 
-          <div>
-            <p> 1. </p>
-            <Image src={BarGraph} alt='BarGraph' />
+          <div className='flex flex-col w-[30%]'>
+            <p className='text-3xl'> 1. </p>
+            <Image src={BarGraph} alt='BarGraph' className='w-full mt-auto'/>
           </div>
-          <div>
-            <h1> Plot Title. </h1>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
+
+          <div className='flex flex-col '>
+            <h1 className='text-2xl'> Plot Title. </h1>
+            <p className='pl-1'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
           </div>
 
         </div>
         
         {/* The Edit Plot Button */}
-        <div>
+        <div className='flex flex-col'>
           <Image src={EditIcon} alt="EditIcon" />
+          <Image src={TrashIcon} alt='TrashIcon' className='mt-auto'/>
         </div>
 
 
