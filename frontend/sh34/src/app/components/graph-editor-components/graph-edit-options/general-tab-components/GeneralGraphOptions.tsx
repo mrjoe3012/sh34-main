6542084991c4353733675e5e935e414d080c1c9e
Matthew <2679216W@student.gla.ac.uk>
2024-01-09 19:08:54 +0000

    Added Plot Option Tab Title Component

--- frontend/sh34/src/app/components/graph-editor-components/graph-edit-options/general-tab-components/GeneralGraphOptions.tsx
+++ frontend/sh34/src/app/components/graph-editor-components/graph-edit-options/general-tab-components/GeneralGraphOptions.tsx
@@ -1,15 +1,22 @@
+import { OptionTabTitle } from "../OptionTabTitle"
 import { PlotTypeOption } from "../option-components/PlotTypeOption"
 import { PlotSizeOption } from "../option-components/PlotSizeOption"
 import { AxisLineOption } from "../option-components/AxisLineOption"
 import { IndicatorOption } from "../option-components/IndicatorOption"
 
+
 export const GeneralGraphOptions = () => {
     return(
-        <div>
-            <PlotTypeOption />
-            <IndicatorOption />
-            <PlotSizeOption />
-            <AxisLineOption />
+        <div className="w-full">
+
+            <OptionTabTitle titleName="General Options" />
+            
+            <div className="flex flex-col items-center w-full">
+                <PlotTypeOption />
+                <IndicatorOption />
+                <PlotSizeOption />
+                <AxisLineOption />
+            </div>
         </div>
     )
 }
