aa01b4907649e9af0118a07f45cf59339f750ec8
Matthew <2679216W@student.gla.ac.uk>
2024-01-09 21:50:35 +0000

    Created Bar Border Settings for Visual Trace Tab

--- frontend/sh34/src/app/components/graph-editor-components/graph-edit-options/visual-tab-components/VisualTraceTab.tsx
+++ frontend/sh34/src/app/components/graph-editor-components/graph-edit-options/visual-tab-components/VisualTraceTab.tsx
@@ -1,8 +1,11 @@
+import { BarBorderOption } from "../option-components/BarBorderOption"
+
 export const VisualTraceTab = () => {
     
+    // Display Either Bar Options, Scatter Options, Pie Options depending on the type of plot currently selected
     return(
-        <div>
-            Trace Options
+        <div className="flex flex-col gap-y-5">
+            <BarBorderOption />
         </div>
     )
 }
