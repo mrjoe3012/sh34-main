922ea476cb39a66547447ce4442845a4a85fb2d5
MatthewW <2679216W@student.gla.ac.uk>
2024-01-10 14:38:22 +0000

    Fixed Error Message Popup

--- frontend/sh34/src/app/components/error-popup-components/ErrorPopup.tsx
+++ frontend/sh34/src/app/components/error-popup-components/ErrorPopup.tsx
@@ -8,7 +8,7 @@ interface ErrorPopupProps {
 export const ErrorPopup = (props: ErrorPopupProps) => {
     return (
         <div className="flex flex-col justify-between w-[400px] h-[400px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border-2 border-RES_ORANGE rounded p-4 z-50">
-            <div className='overflow-y-auto"'>
+            <div className='overflow-y-auto'>
                 <p className='text-3xl mb-5'> Error </p>
                 <p className='text-center font-league flex-grow overflow-auto h-100'>{props.message}</p>
             </div>
