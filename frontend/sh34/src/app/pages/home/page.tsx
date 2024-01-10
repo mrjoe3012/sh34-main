41201ce605bca45c4134f342a55a8049f2a483d6
Erlend Clay <2661388c@student.gla.ac.uk>
2024-01-10 14:22:12 +0000

    added in drop down for sorting templates on the home page, unfinished

--- frontend/sh34/src/app/pages/home/page.tsx
+++ frontend/sh34/src/app/pages/home/page.tsx
@@ -85,12 +85,22 @@ function AssetsHeader(){
           <h2 className='text-lg font-bold'>Last Modified</h2>  
         </div>
 
-        <div className='col-start-7'>
-          <h2 className='text-lg inline'>Sort </h2>  
-          <h2 className='font-bold inline'>A-Z</h2>
+        <div className='col-start-5'>
+          <h2 className='text-lg inline float-right p-1'>Sort </h2>  
         </div>
 
+        <div className='col-start-6'>
+           <select placeholder="Select an option" className="font-medium placeholder-[#ACACAC] h-[35px] w-[150px] bg-[#DCDCDC] rounded-lg flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none">
+            <option value="1">ID</option>
+            <option value="1">Creation date</option>
+            <option value="1">Last modified</option>
+            <option value="1">Name</option>
+          </select>
+        </div>
 
+        <div className='col-start-7'>
+          <h2 className='text-lg inline float-left p-1'>Acending</h2>  
+        </div>
 
       </div>
       <hr className='bg-slate-900'></hr>
