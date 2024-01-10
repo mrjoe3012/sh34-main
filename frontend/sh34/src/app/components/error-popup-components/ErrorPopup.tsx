493933d5132a5dbfe2cb4782ec1ffaa99852d4fa
MatthewW <2679216W@student.gla.ac.uk>
2024-01-10 14:37:34 +0000

    Minor Changes for Error Popup, not nice yet tho

--- frontend/sh34/src/app/components/error-popup-components/ErrorPopup.tsx
+++ frontend/sh34/src/app/components/error-popup-components/ErrorPopup.tsx
@@ -7,13 +7,14 @@ interface ErrorPopupProps {
 
 export const ErrorPopup = (props: ErrorPopupProps) => {
     return (
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border-2 border-gray-300 p-4 z-50">
-            <div className='text-center'>
-                <p>{props.message}</p>
-                <button onClick={props.onClose} className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                    Close
-                </button>
+        <div className="flex flex-col justify-between w-[400px] h-[400px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border-2 border-RES_ORANGE rounded p-4 z-50">
+            <div className='overflow-y-auto"'>
+                <p className='text-3xl mb-5'> Error </p>
+                <p className='text-center font-league flex-grow overflow-auto h-100'>{props.message}</p>
             </div>
+            <button onClick={props.onClose} className="mt-2 px-4 py-2 bg-RES_ORANGE text-white rounded hover:bg-[#f57607] w-[35%]">
+                Close
+            </button>
         </div>
     )
 };
