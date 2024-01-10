c5c51c7d610eea73f7e7f728faaba48467604dce
MatthewW <2679216W@student.gla.ac.uk>
2024-01-10 14:41:54 +0000

    Changed Border Radius of Error Popup

--- frontend/sh34/src/app/components/error-popup-components/ErrorPopup.tsx
+++ frontend/sh34/src/app/components/error-popup-components/ErrorPopup.tsx
@@ -7,7 +7,7 @@ interface ErrorPopupProps {
 
 export const ErrorPopup = (props: ErrorPopupProps) => {
     return (
-        <div className="flex flex-col justify-between w-[400px] h-[400px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border-2 border-RES_ORANGE rounded p-4 z-50">
+        <div className="flex flex-col justify-between w-[400px] h-[400px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border-[4px] border-RES_ORANGE rounded-lg p-4 z-50">
             <div className='overflow-y-auto'>
                 <p className='text-3xl mb-5'> Error </p>
                 <p className='text-center font-league flex-grow overflow-auto h-100'>{props.message}</p>
