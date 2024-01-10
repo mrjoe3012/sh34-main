4f6420fad4c495c620921c31fd1441255cfee282
MatthewW <2679216W@student.gla.ac.uk>
2024-01-10 10:58:27 +0000

    Changed main.py from localhost to 0.0.0.0, made Sidebars buttons actually buttons

--- frontend/sh34/src/app/components/graph-editor-components/sidebar-components/SidebarMainButton.tsx
+++ frontend/sh34/src/app/components/graph-editor-components/sidebar-components/SidebarMainButton.tsx
@@ -9,8 +9,8 @@ interface MainButtonProps {
 export const MainButton = (props: MainButtonProps) => {
 
     return (
-      <div onClick={props.onClick} className={`m-2 text-center text-xl text-white font-semibold h-[60px] rounded-[5px] flex justify-center items-center ${props.buttonColour} `}>
+      <button onClick={props.onClick} className={`m-2 text-center text-xl text-white font-semibold h-[60px] rounded-[5px] flex justify-center items-center ${props.buttonColour} `}>
         {props.buttonName}
-      </div>
+      </button>
     );
   };
