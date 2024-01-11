fa2f2b836c3662970562b46b42a8437ef23734e4
Matthew <2679216W@student.gla.ac.uk>
2024-01-11 19:29:28 +0000

    Altered Styling Layout for Title Options in Labelling Tab

--- frontend/sh34/app/components/graph-editor-components/graph-edit-options/option-components/title-label-components/TitleNameOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/graph-edit-options/option-components/title-label-components/TitleNameOption.tsx
@@ -1,8 +1,8 @@
 export const TitleNameOption = () => {
     return (
         <div className="flex items-center gap-x-2 justify-between">
-            <div>Label</div> 
-            <input placeholder="" className=" font-medium placeholder-[#ACACAC] h-[35px] w-[75%] bg-[#DCDCDC] rounded-xl flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
+            <div className="basis-[30%]">Label</div> 
+            <input placeholder="" className="flex-grow font-medium placeholder-[#ACACAC] h-[35px] w-[75%] bg-[#DCDCDC] rounded-xl flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
         </div>
         
     )
