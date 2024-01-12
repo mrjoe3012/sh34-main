855a4c63b11041b8bf45acf8d3ceaa9335e6f15b
Matthew <2679216W@student.gla.ac.uk>
2024-01-12 16:54:37 +0000

    Changed Visual Appearance of Visual Text Tab Components

--- frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/text-tab/DefaultFontSizeOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/text-tab/DefaultFontSizeOption.tsx
@@ -9,14 +9,15 @@ export const FontSizeOption = () => {
         <div className="bg-[#e6e7eb] py-3 rounded-md">
             < OptionComponentTitle optionName="Default Font Size" />
             <div className="ml-3 flex items-center gap-x-1">
-                    <input 
-                        maxLength={7}
-                        placeholder="" 
-                        className=" font-medium placeholder-[#ACACAC] h-[35px] w-[100px] bg-[#DCDCDC] rounded-xl flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
-                    <div className='flex flex-col gap-y-1'>
-                        <button className="bg-[#DCDCDC] border-[2px] border-[#B3B3B3] p-1 rounded"><Image src={uptriangle} alt="uparrow" className='w-3 h-2 opacity-[65%]'></Image></button>
-                        <button className="bg-[#DCDCDC] border-[2px] border-[#B3B3B3] p-1 rounded"><Image src={downtriangle} alt="uparrow" className='w-3 h-2 opacity-[65%]'></Image></button>
+                <div className="flex items-center gap-x-1">
+                    <div className="flex items-center gap-x-1">
+                        <input maxLength={7} placeholder="px" className="text-right font-medium placeholder-[#ACACAC] h-[35px] w-[100px] bg-[#DCDCDC] rounded-lg flex items-center px-2 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
+                        <div className='flex flex-col h-[35px]'>
+                            <button className="bg-[#DCDCDC] border-[2px] border-[#B3B3B3] rounded mb-auto w-4 h-4 flex justify-center items-center"><Image src={uptriangle} alt="uparrow" className='w-2 h-1 opacity-[65%]'></Image></button>
+                            <button  className="bg-[#DCDCDC] border-[2px] border-[#B3B3B3] rounded w-4 h-4 flex justify-center items-center"><Image src={downtriangle} alt="uparrow" className='w-2 h-1 opacity-[65%]'></Image></button>
+                        </div>
                     </div>
+                </div>
             </div>
         </div>
     )
--- frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/text-tab/DefaultFontTypefaceOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/text-tab/DefaultFontTypefaceOption.tsx
@@ -5,10 +5,10 @@ export const FontTypefaceOption = () => {
     return(
         <div className="bg-[#e6e7eb] py-3 rounded-md">
             < OptionComponentTitle optionName="Default Typeface" />
-            <div className="ml-3">
+            <div className="mx-3">
                     <select 
                         placeholder="" 
-                        className=" font-medium placeholder-[#ACACAC] h-[35px] w-[150px] bg-[#DCDCDC] rounded-lg flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
+                        className=" font-medium placeholder-[#ACACAC] h-[35px] w-full bg-[#DCDCDC] rounded-lg flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
                 </div>
         </div>
     )
