7e2730a9f94658601afa0e7f41c3a3c47bb0fb21
Onur Cay <2662884C@student.gla.ac.uk>
2024-01-12 01:03:41 +0000

    Made basic pie chart work

--- src/backend/plotly_interface.py
+++ src/backend/plotly_interface.py
@@ -104,16 +104,17 @@ def generate_graph(graph_info:dict[str,Any], data_json:dict[str,Any]) -> str:
                               yaxis_title = pascal_split_name(g1.y_axis_name),
                               title=pascal_split_name(g1.title),
                               height = 500)
-            #fig.update_traces(line={ 'width' : 10000})
         case 'pie':
             # A very very simple pie chart
-            fig = px.pie(
-                df, color_discrete_sequence=[g1.colour],
-                title=pascal_split_name(g1.title),
-                height=500
+            trace = go.Pie(
+                labels=df['x'], values=df['y']
+
+
             )
-            fig.update_layout(xaxis_title = g1.x_axis_name,
-                              yaxis_title = pascal_split_name(g1.y_axis_name))
+            fig.update_layout(height=500,
+                              width=500,
+                              title=pascal_split_name(g1.title),
+                              )
         case 'scatter':
             trace = go.Scatter(
                 x=df['x'], y=df['y'],
@@ -133,9 +134,11 @@ def generate_graph(graph_info:dict[str,Any], data_json:dict[str,Any]) -> str:
                 "Please make sure you are entering 'bar', 'line', \
                 'pie', 'scatter' or 'special' as your graph type."
             )
-    df2 = data_extract(data_json, g1.layer_name, first_value)
+
     fig.add_trace(trace)
+
     if g1.layer_type != "":
+        df2 = data_extract(data_json, g1.layer_name, first_value)
         fig.update_layout(yaxis_title = "")
         match g1.layer_type:
             case 'bar':
@@ -169,6 +172,7 @@ def data_extract(data_json:dict[str,Any],name:str,first_value:str) -> pd.DataFra
     :returns: a Pandas Data Frame
     """
 
+
     data_dict = {"x":[],"y":[]}
     monthly_data = data_json["month"]
     graph_name_split = name.split('/')
@@ -178,28 +182,28 @@ def data_extract(data_json:dict[str,Any],name:str,first_value:str) -> pd.DataFra
         level2 = graph_name_split[2]
         named_data = named_data[level2]
 
-    #print(named_data)
-    # asset_data = data_json["asset"]
-    # company_name = asset_data["client_name"]
-    # plant_loc = asset_data["location"]
-    # currency = asset_data["currency"]
-
-
-    #for key,value in data_json.items():
-    #    print(key)
+    for key in named_data:
+        #print("----------------------------------------------------------------------------------")
+        #print((key))
+        pass
 
     #print(data_json.items()[0])
     #print(company_name)
 
-    for entry in named_data:
-        first_key = list(entry.keys())[0]
+    if "aa" == "pie":
+        pass
+    else:
+        for entry in named_data:
+            first_key = list(entry.keys())[0]
+            data_dict["x"].append(entry[first_key])
+            try:
+                #print(first_value)
+                data_dict["y"].append(entry[first_value])
 
-        data_dict["x"].append(entry[first_key])
-        try:
-            data_dict["y"].append(entry[first_value])
-        except KeyError:
-            data_dict["y"].append(None)
-    df = pd.DataFrame(data_dict)
+            except KeyError:
+                data_dict["y"].append(None)
+        df = pd.DataFrame(data_dict)
+    print(df["x"])
     return df
 
 def pascal_split_name(value:str) -> str:
