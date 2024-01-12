086ba3214ec63a100bcbed6f3ca8e8059f26b2bd
Matthew <2679216W@student.gla.ac.uk>
2024-01-12 16:44:02 +0000

    Once Again, fixing graph editor sidebar issues. Name Input Field now properly scales at lower widths

--- frontend/sh34/app/components/graph-editor-components/editor-components/generic-labelling-components/GenericNameOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/generic-labelling-components/GenericNameOption.tsx
@@ -1,9 +1,10 @@
 export const GenericNameOption = () => {
     return (
         <div className="flex items-center gap-x-1">
-            <div className="w-[70px] min-w-[70px]">Name</div> 
-            <input placeholder="" className="flex-grow font-medium placeholder-[#ACACAC] h-[35px] bg-[#DCDCDC] rounded-lg flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
+            <div className="w-[70px] min-w-[70px]">Name</div>
+            <div>
+                <input type="text" placeholder="Plot Title" className="flex-grow font-medium placeholder-[#ACACAC] h-[35px] w-full bg-[#DCDCDC] rounded-lg flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
+            </div>
         </div>
-        
     )
 }
