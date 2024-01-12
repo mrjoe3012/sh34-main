92d81bf92e8ccf3c326e4fe24090302451d72a75
Matthew <2679216W@student.gla.ac.uk>
2024-01-12 17:00:06 +0000

    Altered Appearance of all Colour Editing Components

--- frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/text-tab/DefaultFontColourOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/text-tab/DefaultFontColourOption.tsx
@@ -12,7 +12,7 @@ export const FontColourOption = () => {
                         placeholder="#" 
                         className=" font-medium placeholder-[#ACACAC] h-[35px] w-[100px] bg-[#DCDCDC] rounded-lg flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
                 </div>
-                <div className="w-8 h-8 bg-red-500 rounded-3xl flex border-black border-2"> </div>
+                <div className="w-8 h-8 bg-red-500 rounded-md flex border-black border-2"> </div>
             </div>
         </div>
     )
--- frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/trace-tab/bar-options/BarBorderOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/visual-tab-components/visual-options/trace-tab/bar-options/BarBorderOption.tsx
@@ -9,13 +9,13 @@ export const BarBorderOption = () => {
             <OptionComponentTitle optionName="Bar Border Settings" />
             <div className="flex items-center ml-3 mb-2">
             <div className="mr-2"> Colour </div>
-                <div className="w-8 h-8 bg-red-500 rounded-3xl flex border-black border-2"> </div>
-                <div className="ml-1">
+                <div className="mr-1">
                     <input 
                         maxLength={7}
                         placeholder="#" 
                         className=" font-medium placeholder-[#ACACAC] h-[35px] w-[100px] bg-[#DCDCDC] rounded-lg flex items-center pl-4 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
                 </div>
+                <div className="w-8 h-8 bg-red-500 rounded-md flex border-black border-2"> </div>
             </div>
             <div className="ml-3 flex items-center gap-x-1">
                 <div className="mr-2"> Width </div>
