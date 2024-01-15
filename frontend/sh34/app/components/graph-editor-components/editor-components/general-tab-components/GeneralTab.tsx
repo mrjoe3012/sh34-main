67fbbf2100a84ea366ec22ffefb22d577359899e
Joe <josephagrane@gmail.com>
2024-01-15 13:41:16 +0000

    #75: removed the merge conflict markers

--- frontend/sh34/app/components/graph-editor-components/editor-components/general-tab-components/GeneralTab.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/general-tab-components/GeneralTab.tsx
@@ -3,13 +3,10 @@ import { PlotTypeOption } from "./general-options/PlotTypeOption"
 import { PlotSizeOption } from "./general-options/PlotSizeOption"
 import { AxisLineOption } from "./general-options/AxisLineOption"
 import { IndicatorOption } from "./general-options/IndicatorOption"
-<<<<<<< HEAD
 import { OrientationOption } from "./general-options/OrientationOption"
 import { AxisScaleOption } from "./general-options/AxisScalesOption"
-=======
 import { PlotData } from "@app/modules/db"
 import { WithId } from "mongodb"
->>>>>>> 9ff7593c16ded105c02c42ee33917f834369963d
 
 interface GeneralTabProps {
     selectedIndicator: string,
