9ab8146a1561057e3bf25c9362f3d0eae511ed60
MatthewW <2679216W@student.gla.ac.uk>
2024-01-19 05:07:38 +0000

    Added Axis Line Toggle Functionality - slightly buggy but 5am so

--- frontend/sh34/app/components/graph-editor-components/editor-components/general-tab-components/general-options/AxisLineOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/general-tab-components/general-options/AxisLineOption.tsx
@@ -1,15 +1,23 @@
 import { OptionComponentTitle } from "../../OptionComponentTitle"
 import { GenericCheckboxOption } from "../../generic-components/GenericCheckboxOption"
 
+import configjson from "../../../../../config.json"
+
 export const AxisLineOption = () => {
 
-    const changeXAxisLines = (inputValue: string) => {
+    const changeXAxisLines = (inputValue: boolean) => {
         // Enter logic here for toggling x-axis lines
+
+        configjson["generalOptions"]["displayXAxisLines"] = inputValue
+        console.log(configjson)
         console.log("Toggled x-axis lines to " + inputValue);
     }
 
-    const changeYAxisLines = (inputValue: string) => {
+    const changeYAxisLines = (inputValue: boolean) => {
         // Enter logic here for toggling y-axis lines
+        
+        configjson["generalOptions"]["displayYAxisLines"] = inputValue
+        console.log(configjson)
         console.log("Toggled y-axis lines to " + inputValue);
     }
 
@@ -17,8 +25,8 @@ export const AxisLineOption = () => {
         <div className="bg-[#e6e7eb] py-3 rounded-md"> 
                 <OptionComponentTitle optionName="Axis Lines" />
                 <div className="ml-3">
-                    < GenericCheckboxOption plotFunction={changeXAxisLines} labelName="X-Axis" displayLabel={true} />
-                    < GenericCheckboxOption plotFunction={changeYAxisLines} labelName="Y-Axis" displayLabel={true} />
+                    < GenericCheckboxOption isCheckedOnRender={configjson["generalOptions"]["displayXAxisLines"]} plotFunction={changeXAxisLines} labelName="X-Axis" displayLabel={true} />
+                    < GenericCheckboxOption isCheckedOnRender={configjson["generalOptions"]["displayYAxisLines"]} plotFunction={changeYAxisLines} labelName="Y-Axis" displayLabel={true} />
                 </div>
         </div>
     )
