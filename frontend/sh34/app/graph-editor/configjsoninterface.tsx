9c025e641ac6339ae5d84da822359913a231946b
MatthewW <2679216W@student.gla.ac.uk>
2024-01-20 01:23:14 +0000

    Attempting to Use Context to Hold Config and Pass that config through children. Doesnt currently work atm since when I load config into graph editor page, i cant use useClient since something to do with mongoGB

---
+++ frontend/sh34/app/graph-editor/configjsoninterface.tsx
@@ -0,0 +1,109 @@
+export interface Config {
+    traces: Array<{
+        id: number;
+        name: string;
+        plotType: string;
+        plotIndicator: string;
+        markerColour: string;
+        orientation: string;
+      }>;
+      annotations: Array<any>; 
+      generalOptions: {
+        plotWidth: string;
+        plotHeight: string;
+        displayXAxisLines: boolean;
+        displayYAxisLines: boolean;
+        xAxisScale: string;
+        yAxisScale: string;
+      };
+      labellingOptions: {
+        title: {
+            plotTitle: string;
+            styling: {
+                currentStylingMode: string;
+                defaultFontStyle: {
+                    fontColour: string;
+                    fontSize: number;
+                    typeface: string
+                },
+                customFontStyle: {
+                    fontColour: string;
+                    fontSize: number;
+                    typeface: string;
+                }
+            }
+        },
+        xAxis: {
+            xAxisText: string;
+            styling: {
+                currentStylingMode: string;
+                defaultFontStyle: {
+                    fontColour: string;
+                    fontSize: number;
+                    typeface: string
+                },
+                customFontStyle: {
+                    fontColour: string;
+                    fontSize: number;
+                    typeface: string;
+                }
+            }
+            tickLabels: {
+                tickAngle: number;
+                tickPosition: string;
+                styling: {
+                    currentStylingMode: string;
+                    defaultFontStyle: {
+                        fontColour: string;
+                        fontSize: number;
+                        typeface: string
+                    },
+                    customFontStyle: {
+                        fontColour: string;
+                        fontSize: number;
+                        typeface: string;
+                    }
+                }
+            }
+        },
+        yAxis: {
+            yAxisText: string;
+            styling: {
+                currentStylingMode: string;
+                defaultFontStyle: {
+                    fontColour: string;
+                    fontSize: number;
+                    typeface: string
+                },
+                customFontStyle: {
+                    fontColour: string;
+                    fontSize: number;
+                    typeface: string;
+                }
+            }
+            tickLabels: {
+                tickAngle: number;
+                tickPosition: string;
+                styling: {
+                    currentStylingMode: string;
+                    defaultFontStyle: {
+                        fontColour: string;
+                        fontSize: number;
+                        typeface: string
+                    },
+                    customFontStyle: {
+                        fontColour: string;
+                        fontSize: number;
+                        typeface: string;
+                    }
+                }
+            }
+        }
+      },
+      visualOptions: {
+        colour: {
+            plotBackgroundColourHex: string,
+            plotMarginColourHex: string
+        }
+      }
+}
