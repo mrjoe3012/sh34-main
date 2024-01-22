4b9aad3feac22041c5809e44f30498ea328f2031
MatthewW <2679216W@student.gla.ac.uk>
2024-01-22 01:48:41 +0000

    Added Same validation as prev commit to GenericSizeIncrementerOption

--- frontend/sh34/app/components/graph-editor-components/editor-components/generic-components/GenericSizeIncrementerOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/generic-components/GenericSizeIncrementerOption.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import uptriangle from '@app/images/uptriangle.png'
 import downtriangle from '@app/images/downtriangle.png'
+import { use, useEffect, useState } from "react"
 
 interface GenericSizeIncrementerOptionProps {
     labelName: string,
@@ -11,25 +12,44 @@ interface GenericSizeIncrementerOptionProps {
 
 export const GenericSizeIncrementerOption = (props : GenericSizeIncrementerOptionProps) => {
 
+    const [ inputValue, setInputValue ] = useState("")
+    const [ prevInputValue, setPrevInputValue ] = useState("")
+
     const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-        props.plotFunction(e.currentTarget.value)
+        if (inputValue != prevInputValue) {
+            props.plotFunction(e.currentTarget.value)
+            setPrevInputValue(e.currentTarget.value)
+        }
     }
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
         if (e.key === "Enter") {
-            props.plotFunction(e.currentTarget.value)
+            if (inputValue != prevInputValue) {
+                props.plotFunction(e.currentTarget.value)
+                setPrevInputValue(e.currentTarget.value)
+            }
         }
     };
+
+    const validateInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value)
+    }
+
+    useEffect(()=> {
+        setInputValue(String(props.contentOnRender))
+        setPrevInputValue(String(props.contentOnRender))
+    },[])
 
     return(
         <div className="flex items-center gap-x-1 ml-3 mr-3">
             {props.displayLabel && <div className="w-[70px] min-w-[70px] text-right pr-2"> {props.labelName} </div>}
             <div className="flex items-center gap-x-1">
-                <input  onBlur={handleBlur} 
+                <input  onChange={validateInput}
+                        onBlur={handleBlur} 
                         onKeyDown={handleKeyDown} 
                         maxLength={7} 
                         placeholder="" 
-                        defaultValue={props.contentOnRender}
+                        value={inputValue}
                         className="pl-4 font-medium placeholder-[#ACACAC] h-[35px] w-[100px] bg-[#DCDCDC] rounded-lg flex items-center px-2 border-2 border-[#B3B3B3] focus:ring-2 focus:ring-RES_ORANGE focus:outline-none focus:border-none" />
                 <div className='flex flex-col h-[35px]'>
                     <button className="bg-[#DCDCDC] border-[2px] border-[#B3B3B3] rounded mb-auto w-4 h-4 flex justify-center items-center"><Image src={uptriangle} alt="uparrow" className='w-2 h-1 opacity-[65%]'></Image></button>
