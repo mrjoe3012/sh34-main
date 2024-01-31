a8a09cf411ea87e759cbc7770c36882198db41b4
likahey99 <2661378h@student.gla.ac.uk>
2024-01-31 10:26:59 +0000

    fixed link extending past edit button

--- frontend/sh34/app/components/home-page-components/EditButton.tsx
+++ frontend/sh34/app/components/home-page-components/EditButton.tsx
@@ -6,12 +6,12 @@ interface EditButtonProps {
 
 export const EditButton = (props: EditButtonProps) => {
     return(
-        <Link href={"/template-page/" + props.templateId.toString()}>
-          <div className='text-center w-5/6'>
+      <div className='text-center w-5/6'>
+            <Link href={"/template-page/" + props.templateId.toString()}>
             <div className="justify-center flex rounded-xl p-2 border-black border-2 relative bg-[#616673]">
               <p className="text-slate-50 basis-10/11">Edit</p>
             </div>
-          </div>
         </Link>
+          </div>
     );
 }
