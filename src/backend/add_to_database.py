7208036fc0acab8cb5495c3149d9c8bca87e4e01
Joe <josephagrane@gmail.com>
2024-02-12 13:01:54 +0000

    #107: new ids start from 1 rather tha 0

--- src/backend/add_to_database.py
+++ src/backend/add_to_database.py
@@ -26,7 +26,7 @@ def get_new_id(collection):
         last_id = max(data['_id'] for data in collection_data)
         return last_id + 1
     else:
-        return 0
+        return 1
 
 def get_next_order(template: dict[str, Any]) -> int:
     """
@@ -40,7 +40,7 @@ def get_next_order(template: dict[str, Any]) -> int:
     if len(plots) > 0:
         return max(plot['order'] for plot in plots) + 1
     else:
-        return 0
+        return 1
 
 def add_template(name, description, tags):
     """
