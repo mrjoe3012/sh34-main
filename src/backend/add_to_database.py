15a94f8da6542f575b4189992b626be689b57aa8
Joe <josephagrane@gmail.com>
2024-02-12 11:48:04 +0000

    #107: backend code for adding plots to the database

--- src/backend/add_to_database.py
+++ src/backend/add_to_database.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from .database import get_client, load_collection
+from .database import get_client, load_collection, load_plots_from_template, load_templates, PLOT_COLLECTION_NAME, TEMPLATE_COLLECTION_NAME
+from typing import Any
 
 def connect_to_database():
     """
@@ -13,10 +14,20 @@ def get_new_id(collection):
     """
     Given a collection, return the id that the next element added to the collection should have
     """
-    print("hello World")
     collection_data = load_collection(collection, {})
-    last_id = collection_data[-1]['_id']
+    last_id = max([data['_id'] for data in collection_data])
     return last_id + 1
+
+def get_next_order(template: dict[str, Any]) -> int:
+    """
+    Given a template id, figure out how many templates it has
+    and return the ordering of a newly added plot.
+
+    :param template: The template's id.
+    :returns: The next ordering value.
+    """
+    plots = load_plots_from_template(template)
+    return max([plot['order'] for plot in plots]) + 1
 
 def add_template(name, description, tags):
     """
@@ -26,7 +37,7 @@ def add_template(name, description, tags):
     collection = "Templates_Data"
     current_date_time = datetime.now()
     dt_string = current_date_time.strftime("%d/%m/%Y")
-    print(dt_string)
+    print("Adding template")
 
     document = {"_id": get_new_id(collection), # Increment of last elements ID
                 "PlotArray": [], #Initially has 0 Plots
@@ -41,17 +52,27 @@ def add_template(name, description, tags):
     print(f"Inserted document id: {result.inserted_id}")
     return document
 
-def add_plot():
+def add_plot(template_id: str, config_file: dict[str, Any]) -> dict[str, Any]:
     """
     Add a plot to the database
+    
+    :param template_id: The template it's being added to.
+    :param config_file: The plot's configuration.
+    :returns: The added plot.
     """
-    db = connect_to_database()
-    collection = db["Plots_Data"]
+    id = get_new_id(PLOT_COLLECTION_NAME)
+    template = load_templates({'_id' : template_id})[0]
+    order = get_next_order(template)
 
-    document = {"_id": get_new_id(db,collection), #Increment of last elements ID
-                "config_file": "", #Include Name and Graph Type. 
-                "order": "" #Increment of last plot's order within this plots template
+    document = {"_id": id,
+                "config_file": config_file, 
+                "order": order
                 }
 
+    db = connect_to_database()
+    collection = db[PLOT_COLLECTION_NAME]
     result = collection.insert_one(document)
-    print(f"Inserted document id: {result.inserted_id}")
+    collection = db[TEMPLATE_COLLECTION_NAME]
+    template['PlotArray'].append(id)
+    collection.update_one({'_id' : template_id}, {"$set" : template})
+    return document
