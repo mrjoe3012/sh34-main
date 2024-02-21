316b71932a838043e9ea9ddfaf6ba2922cd3a012
MatthewW <2679216W@student.gla.ac.uk>
2024-02-21 11:59:26 +0000

    Attempt 3 to Fix Pipeline

--- frontend/sh34/app/components/graph-editor-components/editor-components/trace-tab-components/trace-options/marker-colour-components/MarkerColourOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/trace-tab-components/trace-options/marker-colour-components/MarkerColourOption.tsx
@@ -2,16 +2,10 @@ import { OptionComponentTitle } from "@app/components/graph-editor-components/ed
 import { MarkerColourTabSwitcher } from "@app/components/graph-editor-components/editor-components/trace-tab-components/trace-options/marker-colour-components/MarkerColourTabSwitcher";
 import { useState } from 'react';
 import { MarkerConstantOption } from "@app/components/graph-editor-components/editor-components/trace-tab-components/trace-options/marker-colour-components/MarkerConstantOption";
+import { TraceType } from "@app/modules/Config";
 
 interface MarkerColourOptionProps {
-    trace: {
-        id: number,
-        name: string,
-        plotType: string,
-        plotIndicator: string,
-        markerColour: string,
-        orientation: string
-    }
+    trace: TraceType;
     plotFunctionConstant: (inputValue: string) => void;
     plotFunctionScale: (inputValue: string) => void;
 }
