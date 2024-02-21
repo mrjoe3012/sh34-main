f32adc9fabb131a806be02a49f5d23cf811a6979
MatthewW <2679216W@student.gla.ac.uk>
2024-02-21 12:39:05 +0000

    Fixed Styling Issues with Generic Components Spacing

--- frontend/sh34/app/components/graph-editor-components/editor-components/generic-components/GenericColourOption.tsx
+++ frontend/sh34/app/components/graph-editor-components/editor-components/generic-components/GenericColourOption.tsx
@@ -50,21 +50,21 @@ export const GenericColourOption = (props : GenericColourOptionProps) => {
       setHexValue(props.contentOnRender)
       setPrevHexValue(props.contentOnRender)
     }, [])
-  
+
     return (
       <>
       <div className="flex items-center ml-3 mr-3 ">
-        {props.displayLabel && <div className="w-[60px] min-w-[60px] text-left pr-2">{props.labelName}</div>}
+        {props.displayLabel && <div className="w-[70px] min-w-[70px] text-right pr-2">{props.labelName}</div>}
         <div className="flex items-center gap-x-1">
         <button onClick={toggleColourPicker} className={`w-8 h-8 rounded-md flex border-gray-300 border-2`} style={{ backgroundColor: `#${hexValue}` }}> </button>
           <div className="flex border-2 border-[#B3B3B3] bg-[#DCDCDC] rounded-lg focus-within:border-RES_ORANGE h-[35px] w-[100px]">
             <span className="flex items-center pl-3 pr-1 text-gray-700">#</span>
-            <input 
-              type="text" 
-              className="bg-[#DCDCDC] w-full flex-1 rounded-r-lg focus:outline-none" 
-              placeholder="" 
-              maxLength={6} 
-              pattern="[0-9A-Fa-f]" 
+            <input
+              type="text"
+              className="bg-[#DCDCDC] w-full flex-1 rounded-r-lg focus:outline-none"
+              placeholder=""
+              maxLength={6}
+              pattern="[0-9A-Fa-f]"
               value={hexValue}
               onChange={validateInputFromEvent}
               onBlur={handleBlur}
@@ -72,7 +72,7 @@ export const GenericColourOption = (props : GenericColourOptionProps) => {
             />
           </div>
         </div>
-        
+
       </div>
       {showColourPicker && < ColourPickerPopup hexValue={hexValue} validateInput={validateInputFromPicker}/>}
       </>
