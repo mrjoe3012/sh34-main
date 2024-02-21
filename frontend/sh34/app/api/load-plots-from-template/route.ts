4d814e8dcd8a508c733024edae88d61b6258db9b
Joe <josephagrane@gmail.com>
2024-02-21 16:24:33 +0000

    #119: added api route for loading templates for a plot

---
+++ frontend/sh34/app/api/load-plots-from-template/route.ts
@@ -0,0 +1,32 @@
+import { loadPlotsFromTemplate, loadTemplates } from "@app/modules/db";
+import { WithId } from "mongodb";
+import { NextRequest } from "next/server";
+import { PlotData } from "plotly.js";
+
+export const revalidate = 0;
+
+interface RequestData {
+    id: number,
+};
+
+export interface LoadPlotsFromTemplatesResponseData {
+    plots: WithId<PlotData>[],
+};
+
+export async function POST(request: NextRequest) {
+    const data: RequestData = await request.json();
+    try {
+        const template = (await loadTemplates({'_id' : data.id}))[0];
+        const plots = await loadPlotsFromTemplate(template);
+        return Response.json({
+            'status' : 200,
+            'plots' : plots,
+        });
+    } catch (e) {
+        console.error(`Failed to serve plots. Error: ${e}`);
+        return Response.json({
+            'status' : 400,
+            'error' : `Failed to serve plots. Error: ${e}`,
+        });
+    }
+}
