8a3b1f424d58dcf05722c6502c28ad4b215a59ca
Joe <josephagrane@gmail.com>
2024-02-21 16:28:24 +0000

    #119: added module for loading plots from frontend

---
+++ frontend/sh34/app/modules/PlotLoader.tsx
@@ -0,0 +1,36 @@
+import { LoadPlotsFromTemplatesResponseData } from "@app/api/load-plots-from-template/route";
+import { request } from "http";
+import { WithId } from "mongodb";
+import { PlotData } from "plotly.js";
+
+export class PlotLoader {
+    constructor() {
+
+    }
+
+    async loadPlotsFromTemplate(id: number): Promise<WithId<PlotData>[]> {
+        const dbApiEndpoint = '/api/db/load-plots-from-template';
+        try {
+            const response = await fetch(dbApiEndpoint, {
+                'method' : 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify({
+                    id: id,
+                }),
+            });
+            if (response.status != 200)
+                throw new Error(`Failed to fetch plots for template id: ${id}.`);
+            const data: LoadPlotsFromTemplatesResponseData = await request.json();
+            return data.plots;
+        } catch(e) {
+            console.error(e);
+            return [];
+        }
+    }
+
+    async loadPlots(filter: Record<string, any>): Promise<WithId<PlotData>[]> {
+        return [];
+    }
+}
