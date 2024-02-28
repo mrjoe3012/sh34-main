171a0c30dcc1b58c7d693ab3eb576db3dda5f99d
MatthewW <2679216W@student.gla.ac.uk>
2024-02-28 18:21:50 +0000

    Added debounce to name and description text inputs as to not call API for every character change

--- frontend/sh34/app/components/template-page-components/info-page-components/TemplateNameInput.tsx
+++ frontend/sh34/app/components/template-page-components/info-page-components/TemplateNameInput.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { WithId } from "mongodb";
 import { TemplateData } from "@app/modules/db";
 import { Dispatch, SetStateAction } from 'react';
@@ -9,21 +9,42 @@ interface TemplateNameInputProps {
     setTemplate: Dispatch<SetStateAction<WithId<TemplateData>>>;
 };
 
+function useDebounce(value: string, delay:number) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+
+      return () => {
+        clearTimeout(handler);
+      };
+    }, [value, delay]);
+
+    return debouncedValue;
+}
+
+
 export const TemplateNameInput = (props: TemplateNameInputProps) => {
     const [name, setName] = useState(props.template.Name);
-    const onNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-        props.setTemplate(currentTemplate => ({
-            ...currentTemplate,
-            Name: event.target.value
-        }));
-    };
+    const debouncedName = useDebounce(name, 500);
+
+    useEffect(()=>{
+        if (debouncedName) {
+            props.setTemplate(currentTemplate => ({
+                ...currentTemplate,
+                Name: debouncedName
+            }));
+        }
+    },[debouncedName])
+
     return (
         <div>
             <p className="text-2xl"> Name </p>
             <input
             value={name}
-            onChange={onNameChange}
+            onChange={(e)=> setName(e.target.value)}
             className="text-xl font-medium placeholder-[#ACACAC] h-[60px] w-[500px] bg-[#E7E7E7] rounded-xl flex items-center pl-4 outline-none focus:ring-2 focus:ring-RES_ORANGE" />
         </div>
     );
