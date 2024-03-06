2ef73d7f614d803b1f843ccc30f4e12feb80e540
MatthewW <2679216W@student.gla.ac.uk>
2024-03-06 13:42:37 +0000

    Added Arrow to Return Button on Graph Editor

--- frontend/sh34/app/components/graph-editor-components/sidebar-components/ReturnButton.tsx
+++ frontend/sh34/app/components/graph-editor-components/sidebar-components/ReturnButton.tsx
@@ -1,14 +1,19 @@
 import { useConfig } from "@app/graph-editor/ConfigContext";
 import Link from "next/link";
+import Image from "next/image";
+import LongLeftArrow from "@app/images/long-arrow-left-icon.svg"
 
 
 export const ReturnButton = () => {
-    const {template} = useConfig(); 
+    const {template} = useConfig();
     if (template === null) return <div></div>
     return (
         <div className='p-2'>
             <Link href={`/template-page/${template._id.toString()}`}>
-                <p>Return to Template</p>
+                <div className="flex items-center gap-x-2">
+                    <Image src={LongLeftArrow} alt="arrow" className="w-4 h-4" />
+                    <p>Return to Template</p>
+                </div>
             </Link>
         </div>
     );
