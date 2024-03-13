47aed22cf04b5f30fda97180e0bf6b6aed70d1a8
MatthewW <2679216W@student.gla.ac.uk>
2024-03-13 14:15:52 +0000

    Can Now Properly Sort by Last Modified Field

--- frontend/sh34/app/components/home-page-components/AssetElement.tsx
+++ frontend/sh34/app/components/home-page-components/AssetElement.tsx
@@ -3,14 +3,39 @@ import { TemplateData } from "@app/modules/db";
 import { WithId } from "mongodb";
 import { EditButton } from "./EditButton";
 import { ExportButton } from "@app/components/home-page-components/ExportButton";
-import LastModified from "@app/components/home-page-components/LastModified";
 
 
 interface AssetElementProps {
   template: WithId<TemplateData>;
 };
 
+function timeAgo(dateString: string) {
+  if (new Date(dateString) > new Date() || isNaN(new Date(dateString).getTime())) {
+    return "Invalid Date"
+  }
+  const date = new Date(dateString);
+  const now = new Date();
+  const seconds = Math.round((now.getTime() - date.getTime()) / 1000);
+  const minutes = Math.round(seconds / 60);
+  const hours = Math.round(minutes / 60);
+  const days = Math.round(hours / 24);
+  const months = Math.round(days / 30);
+  const years = Math.round(days / 365);
 
+  if (seconds < 60) {
+    return 'Just Now';
+  } else if (minutes < 60) {
+    return `${minutes} Minutes Ago`;
+  } else if (hours < 24) {
+    return `${hours} Hours Ago`;
+  } else if (days < 30) {
+    return `${days} Days Ago`;
+  } else if (months < 12) {
+    return `${months} Months Ago`;
+  } else {
+    return `${years} Years Ago`;
+  }
+}
 
 export const AssetElement = (props: AssetElementProps) => {
   return(
@@ -30,7 +55,7 @@ export const AssetElement = (props: AssetElementProps) => {
           </div>
 
           <div className="col-span-1">
-            < LastModified template={props.template} />
+            {timeAgo(props.template.LastModified)}
           </div>
 
           <div className='col-span-1 col-start-6 px-0.5'> {/* breakpoints needed to stop it from going over 2 lines on smaller screen OR more elegant solution */}
