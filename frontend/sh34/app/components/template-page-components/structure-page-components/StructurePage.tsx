5161b7eafc850d84303841f5c5eef7ac12cd1a4f
MatthewW <2679216W@student.gla.ac.uk>
2024-03-13 12:35:30 +0000

    Added Loading Icon to Template Structure Page when Plot Elements Haven't Loaded

--- frontend/sh34/app/components/template-page-components/structure-page-components/StructurePage.tsx
+++ frontend/sh34/app/components/template-page-components/structure-page-components/StructurePage.tsx
@@ -2,6 +2,7 @@ import { PlotElement } from "./PlotElement"
 import { PlotSearchInput } from "./PlotSearchInput";
 import { NewPlotButton } from "./NewPlotButton";
 import { useTemplatePageContext } from "@app/template-page/TemplatePageContext";
+import ReactLoading from "react-loading"
 
 export const StructurePage = () => {
     const {plots, setPlots} = useTemplatePageContext();
@@ -18,14 +19,23 @@ export const StructurePage = () => {
             </div>
 
             <div className='flex flex-col gap-y-8 items-center'>
-                {sortedPlots.map((plot) =>{
-                    return (<PlotElement
-                        key={plot._id}
-                        plot={plot}
-                        plots={plots}
-                        setPlots={setPlots}
-                    />)
-                })}
+
+            {sortedPlots.length ==0 ? (
+                    // Display Loading text if plots are not initialized.
+                    <ReactLoading type="spin" color="black" height={30} width={30}/>
+                ) : (
+                    // Render PlotElement components if plots are initialized.
+                    sortedPlots.map((plot) => {
+                        return (
+                            <PlotElement
+                                key={plot._id}
+                                plot={plot}
+                                plots={plots}
+                                setPlots={setPlots}
+                            />
+                        )
+                    })
+                )}
             </div>
         </div>
       )
